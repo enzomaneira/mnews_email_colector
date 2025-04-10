62df7a172a4b1f5b755344a80c0151938084fc59
enzomaneira <enzo@maneira.com>
2025-04-10 19:28:23 -0300

    feat: adiciona configuração de CORS

---
+++ newsletter-application/src/main/java/com/maneira/newsletter/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.maneira.newsletter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://3.90.201.215:3000") // ou "*" se quiser liberar geral (menos seguro)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
